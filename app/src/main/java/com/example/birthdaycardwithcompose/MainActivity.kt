a9e5c1ee364bb07a32d2371f715982136ceaea28
Suhail <Suhail@vajro.com>
2022-05-26 18:16:11 +0530

    *Tried with row and column

--- app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.birthdaycardwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthdaycardwithcompose.ui.theme.BirthdayCardWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +36,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingForBirthdayCard(message : String){
-
+    Column {
+        Text(text = message, modifier = Modifier.padding(12.dp))
+        Text(text = "Hello world, Test")
+    }
 }
 
 @Preview(showBackground = true)

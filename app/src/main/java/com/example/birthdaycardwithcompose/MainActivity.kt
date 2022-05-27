2017366b8a3e2d868161fec4d10e44d282291eac
Suhail <Suhail@vajro.com>
2022-05-27 19:07:57 +0530

    *Added Image to the compose *Uses Box container

--- app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.birthdaycardwithcompose
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingForBirthdayCard("Android")
+                    GreetingForBirthdayCardWithImage("Android","Suhail")
                 }
             }
         }
@@ -35,10 +36,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingForBirthdayCard(message : String){
+fun GreetingForBirthdayCardWithImage(message: String, from : String){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null,
+        modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+        contentScale = ContentScale.Crop)
+        GreetingForBirthdayCard(message = message, from = from)
+    }
+
+
+}
+
+@Composable
+fun GreetingForBirthdayCard(message : String , from : String){
     Column {
-        Text(text = message, modifier = Modifier.padding(12.dp))
-        Text(text = "Hello world, Test")
+        Text(text = message, modifier = Modifier.padding(5.dp))
+        Text(text = from)
     }
 }
 
@@ -46,6 +60,6 @@ fun GreetingForBirthdayCard(message : String){
 @Composable
 fun DefaultPreview() {
     BirthdayCardWithComposeTheme {
-        GreetingForBirthdayCard("Suhail")
+        GreetingForBirthdayCardWithImage("Happy Birthday!!!","Suhail P A")
     }
 }

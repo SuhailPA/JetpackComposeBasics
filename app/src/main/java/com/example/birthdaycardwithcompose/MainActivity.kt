af82c2a0eeac06cb7f3e04c6c9ba15f0fca04976
Suhail <Suhail@vajro.com>
2022-05-30 10:23:58 +0530

    *Uses modifier wrapContentWidth for alignments in text

--- app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardwithcompose/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,8 +53,15 @@ fun GreetingForBirthdayCardWithImage(message: String, from : String){
 @Composable
 fun GreetingForBirthdayCard(message : String , from : String){
     Column {
-        Text(text = message, modifier = Modifier.padding(5.dp))
-        Text(text = from)
+        Text(text = message,
+            modifier = Modifier.fillMaxWidth().
+            wrapContentWidth(align = Alignment.Start).
+            padding(16.dp,5.dp,16.dp,0.dp),
+         )
+        Text(text = from,
+        modifier = Modifier.fillMaxWidth().
+        wrapContentWidth(Alignment.End)
+            .padding(start = 16.dp, end = 16.dp))
     }
 }
 
@@ -60,6 +69,8 @@ fun GreetingForBirthdayCard(message : String , from : String){
 @Composable
 fun DefaultPreview() {
     BirthdayCardWithComposeTheme {
-        GreetingForBirthdayCardWithImage("Happy Birthday!...Wish you all the best","Suhail P A")
+        GreetingForBirthdayCardWithImage(stringResource(R.string.happy_birthday_string),
+                    stringResource(R.string.wishes_from)
+                )
     }
 }
